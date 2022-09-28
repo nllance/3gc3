7872cf4575e67c4b7378478291582161bb9e00a1
Lawrence Gu <nllance@pm.me>
2022-09-28 10:48:07 -0400

    assginment 0 test

---
+++ as0/build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/assignment0.cpp.o"
+  "CMakeFiles/test.dir/assignment0.cpp.o.d"
+  "CMakeFiles/test.dir/glad.c.o"
+  "CMakeFiles/test.dir/glad.c.o.d"
+  "test"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
